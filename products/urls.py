e6da284f43dcca5f403accce336e7239db304673
tisha-13 <tishav1303@gmail.com>
2026-07-03 11:54:11 +0530

    Built a Django e-commerce website with shopping cart, search, product details and Docker support

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "products"
 
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new)
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+
+    path("cart/", views.cart, name="cart"),
+    path("add-to-cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("remove-from-cart/<int:product_id>/", views.remove_from_cart, name="remove_from_cart"),
+
+    path("product/<int:product_id>/", views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,78 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 
+def home(request):
+    featured_products = Product.objects.all()[:3]
 
-# Create your views here.
+    cart = request.session.get('cart', [])
+
+    return render(request, "home.html", {
+        "featured_products": featured_products,
+        "cart_count": len(cart)
+    })
+
+
 def index(request):
-    # return HttpResponse('Hello World')
-    products = Product.objects.all()
-    return render(request, 'index.html',
-                  {'products': products})
+    search = request.GET.get('search')
+
+    if search:
+        products = Product.objects.filter(name__icontains=search)
+    else:
+        products = Product.objects.all()
+
+    cart = request.session.get('cart', [])
+
+    return render(request, 'index.html', {
+        'products': products,
+        'cart_count': len(cart)
+    })
 
 
 def new(request):
     return HttpResponse('Welcome to PyShop New Arrivals')
 
+
+# Show cart page
+def cart(request):
+    cart = request.session.get('cart', [])
+
+    products = Product.objects.filter(id__in=cart)
+
+    total = sum(product.price for product in products)
+
+    return render(request, 'cart.html', {
+        'products': products,
+        'total': total,
+        'cart_count': len(cart)
+    })
+# Add product to cart
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', [])
+
+    if product_id not in cart:
+        cart.append(product_id)
+
+    request.session['cart'] = cart
+
+    return redirect('products:cart')
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', [])
+
+    if product_id in cart:
+        cart.remove(product_id)
+
+    request.session['cart'] = cart
+
+    return redirect('products:cart')
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    cart = request.session.get('cart', [])
+
+    return render(request, 'product_detail.html', {
+        'product': product,
+        'cart_count': len(cart)
+    })
